5885ec55449a7abc426fb08619786b28c90e5728
Maciej Łebkowski <m.lebkowski@gmail.com>
2023-06-15 21:32:49 +0200

    I forgot to update phpunit

--- tests/PhpUnit/Domain/Meeting/MeetingTest.php
+++ tests/PhpUnit/Domain/Meeting/MeetingTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Domain\Meeting;
 
+use App\Common\Clock\RealClock;
 use App\Domain\Meeting\Problems\MeetingFullException;
 use App\Domain\User\UserMother;
 use PHPUnit\Framework\TestCase;
@@ -21,28 +22,28 @@ class MeetingTest extends TestCase
     public function test it is done after end passed(): void
     {
         $sut = MeetingMother::withPastEndDate();
-        $actual = $sut->getStatus();
+        $actual = $sut->getStatus(new RealClock());
         self::assertSame(MeetingStatus::Done, $actual);
     }
 
     public function test it is in session if the end date has not passed yet(): void
     {
         $sut = MeetingMother::inSession();
-        $actual = $sut->getStatus();
+        $actual = $sut->getStatus(new RealClock());
         self::assertSame(MeetingStatus::InSession, $actual);
     }
 
     public function test it reports it is full(): void
     {
         $sut = MeetingMother::withMaxParticipants();
-        $actual = $sut->getStatus();
+        $actual = $sut->getStatus(new RealClock());
         self::assertSame(MeetingStatus::Full, $actual);
     }
 
     public function test it is open to registration(): void
     {
         $sut = MeetingMother::some();
-        $actual = $sut->getStatus();
+        $actual = $sut->getStatus(new RealClock());
         self::assertSame(MeetingStatus::OpenToRegistration, $actual);
     }
 }
