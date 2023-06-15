857b0da82bb7d1a057089e55f979189d6959e0ff
Maciej Łebkowski <m.lebkowski@gmail.com>
2023-06-15 21:09:55 +0200

    oops, drop those comments

--- src/Domain/Meeting/MeetingStatus.php
+++ src/Domain/Meeting/MeetingStatus.php
@@ -6,9 +6,6 @@ namespace App\Domain\Meeting;
 
 enum MeetingStatus: string
 {
-// "open to registration" - if meeting has fewer than 5 participants and didn't start yet
-// "full" - if there are 5 participants, but it didn't start yet
-// "in session" - if it has started but didn't finish
     case OpenToRegistration = "open to registration";
     case Full = "full";
     case InSession = "in session";
